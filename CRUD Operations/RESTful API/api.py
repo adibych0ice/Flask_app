0a03d673fda6f08e6399306962117caddb3234fb
Aditya Nittala <adityann97@gmail.com>
2023-08-16 09:55:53 -0400

    commit

--- CRUD Operations/RESTful API/api.py
+++ CRUD Operations/RESTful API/api.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class FirstClass(Resource):
     def get(self):
-        return {'Hello World'}
+        return {"data":'Hello World'}
 
 api.add_resource(FirstClass,'/helloworld')
 if __name__ == "__main__":
--- CRUD Operations/RESTful API/test.py
+++ CRUD Operations/RESTful API/test.py
@@ -1,2 +1,6 @@
 import requests
 
+baseurl = "https:/127.0.0.1:5000/"
+
+response = requests.get(baseurl+"helloworld")
+print(response.json())
