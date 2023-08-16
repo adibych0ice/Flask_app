cfac1e76ded532bb39ca9d92746c7f081db29aa9
Aditya Nittala <adityann97@gmail.com>
2023-08-16 13:12:27 -0400

    commiting changes made to api.py and test.py

--- CRUD Operations/RESTful API/api.py
+++ CRUD Operations/RESTful API/api.py
@@ -1,13 +1,50 @@
 from flask import Flask
 from flask_restful import Resource,Api
-
+import psycopg2
+from psycopg2.extras import DictCursor
+import datetime
 app = Flask(__name__)
 api = Api(app)
+names = {"frederick":"He is a male","8899":"This is a random number "}
+class GetTableDetails(Resource):
 
-class FirstClass(Resource):
-    def get(self):
-        return {"data":'Hello World'}
 
-api.add_resource(FirstClass,'/helloworld')
+    def tabledetails(self,pwd,tablename,schemaname):
+        
+        conn = psycopg2.connect(database = 'postgres',user='postgres',password=pwd, host='localhost',port='5432')
+        curs = conn.cursor()
+
+        tabledetails = "SELECT column_name,data_type from information_schema.columns WHERE table_schema = %s AND table_name = %s "
+        curs.execute(tabledetails,(schemaname,tablename))
+        columns = curs.fetchall()
+        collist = [{"columnname":name,"datatype":dattype}for name,dattype in columns]
+        return  collist, conn
+    
+    def querytable(self,schemaname,colname, queryval,conn,collist,tabname):
+        curs = conn.cursor(cursor_factory=DictCursor)
+
+        colstring = ",".join([i["columnname"] for i in collist])
+
+        querystring = f"SELECT {colstring} FROM {schemaname}.{tabname} WHERE {colname} = %s"
+
+        curs.execute(querystring, (queryval,))
+        filterresult = curs.fetchall()
+
+        colnames = [x['columnname'] for x in collist]
+        result = [{col: (value.isoformat() if isinstance(value, datetime.date) else value) for col, value in zip(colnames, row)} for row in filterresult]
+        return result, 200
+    
+    def get(self, password,tablename,schemaname,columnname,colfilter):
+        columnlist, connection = self.tabledetails(password,tablename,schemaname)
+        filterresult, statcode = self.querytable(schemaname,columnname,colfilter,connection,columnlist,tablename)
+
+        return filterresult,statcode
+
+
+         
+#The <> is to pass the paramater that will be passed when pinging the endpoint
+api.add_resource(GetTableDetails,'/tabdetails/<string:password>/<string:tablename>/<string:schemaname>/<string:columnname>/<string:colfilter>')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- CRUD Operations/RESTful API/test.py
+++ CRUD Operations/RESTful API/test.py
@@ -1,6 +1,42 @@
-import requests
+from flask import Flask
+from flask_restful import Resource,Api
+import psycopg2
+from psycopg2.extras import DictCursor
+import datetime
+from flask import jsonify
+app = Flask(__name__)
 
-baseurl = "https:/127.0.0.1:5000/"
+def tabledetails(pwd,tablename,schemaname,tabname,colname,queryval):
+        
+        conn = psycopg2.connect(database = 'postgres',user='postgres',password=pwd, host='localhost',port='5432')
+        curs = conn.cursor()
 
-response = requests.get(baseurl+"helloworld")
-print(response.json())
+        tabledetails = "SELECT column_name,data_type from information_schema.columns WHERE table_schema = %s AND table_name = %s "
+        curs.execute(tabledetails,(schemaname,tablename))
+        columns = curs.fetchall()
+        collist = [{"columnname":name,"datatype":dattype}for name,dattype in columns]
+
+        curs = conn.cursor(cursor_factory=DictCursor)
+
+        colstring = ",".join([i["columnname"] for i in collist])
+
+        querystring = f"SELECT {colstring} FROM {schemaname}.{tabname} WHERE {colname} = %s"
+
+        curs.execute(querystring, (queryval,))
+        filterresult = curs.fetchall()
+        # result = []
+        # for row in filterresult:
+        #     row_dict = {}
+        #     for i, col in enumerate(collist):
+        #         value = row[i]
+        #         if isinstance(value, datetime.date):
+        #             value = value.isoformat()
+        #         row_dict[col["columnname"]] = value
+        #     result.append(row_dict)
+        colnames = [x['columnname'] for x in collist]
+        result = [{col: (value.isoformat() if isinstance(value, datetime.date) else value) for col, value in zip(colnames, row)} for row in filterresult]
+        return result, 200
+
+result, stat_code = tabledetails("Tachyon_9667","users","decistech","users","email","ajackson@example.net")
+
+print(result)
