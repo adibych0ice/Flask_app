095e23fe05615be32467f1d434647fb94286b17f
Aditya Nittala <adityann97@gmail.com>
2023-08-15 20:18:44 -0400

    changes and new package installation

--- CRUD Operations/RESTful API/api.py
+++ CRUD Operations/RESTful API/api.py
@@ -4,8 +4,10 @@ from flask_restful import Resource,Api
 app = Flask(__name__)
 api = Api(app)
 
+class FirstClass(Resource):
+    def get(self):
+        return {'Hello World'}
 
-
-
+api.add_resource(FirstClass,'/helloworld')
 if __name__ == "__main__":
     app.run(debug=True)
