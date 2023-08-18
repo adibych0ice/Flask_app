a1b4c6c3baf0deb5670b416f7dde92900816384f
Aditya Nittala <adityann97@gmail.com>
2023-08-18 11:25:59 -0400

    commit

--- app/YouTube Tutorials/api.py
+++ app/YouTube Tutorials/api.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api,Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__);
-
+db = SQLAlchemy(app);
+app.config['SQLALCHEMY_DATABASE_URI']
 api = Api(app);
 
 names = {
